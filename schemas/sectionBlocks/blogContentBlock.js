7077630969b60130a81380cc688281074315fe27
Werner Glinka <werner@glinka.co>
2022-12-26 17:30:19 -0600

    added inline code and numbered lists to rich text blocks

--- schemas/sectionBlocks/blogContentBlock.js
+++ schemas/sectionBlocks/blogContentBlock.js
@@ -25,12 +25,19 @@ export default defineType({
             {title: 'H4', value: 'h4'},
             {title: 'Quote', value: 'blockquote'}
           ],
-          lists: [{title: 'Bullet', value: 'bullet'}],
+          lists: [
+            {title: 'Bullet', value: 'bullet'},
+            {title: 'Numbered', value: 'number'},
+          ],
           // Marks let you mark up inline text in the block editor.
           marks: {
             // Decorators usually describe a single property – e.g. a typographic
             // preference or highlighting by editors.
-            decorators: [{title: 'Strong', value: 'strong'}, {title: 'Emphasis', value: 'em'}],
+            decorators: [
+              {title: 'Strong', value: 'strong'}, 
+              {title: 'Emphasis', value: 'em'},
+              {title: 'Code', value: 'code'},
+            ],
             // Annotations can be any object structure – e.g. a link or a footnote.
             annotations: [
               {
