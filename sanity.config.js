75913a70f859a459258c8e34f427e557cb4a0039
Werner Glinka <werner@glinka.co>
2022-12-27 13:20:49 -0600

    chnaged builkd hook id

--- sanity.config.js
+++ sanity.config.js
@@ -26,13 +26,13 @@ export default defineConfig({
               {
                 title: 'Sanity Studio',
                 apiId: '7899622d-3925-4716-824d-6ae101ed21d8',
-                buildHookId: 'https://api.netlify.com/build_hooks/63ab3fbf3258e44bbb4acba1',
+                buildHookId: '63ab3fbf3258e44bbb4acba1',
                 name: 'ruhrpott-sanity-studio',
               },
               {
                 title: 'Website',
                 apiId: '7899622d-3925-4716-824d-6ae101ed21d8',
-                buildHookId: 'https://api.netlify.com/build_hooks/63ab3fbf3258e44bbb4acba1',
+                buildHookId: '63ab3fbf3258e44bbb4acba1',
                 name: 'ruhrpott-sanity-website',
                 url: 'https://ruhrpott.netlify.app/',
               }
