85be70f53edce3df0d2cc60ce93f81f187517e6e
Werner Glinka <werner@glinka.co>
2022-12-27 12:58:24 -0600

    added netlify build hook

--- sanity.config.js
+++ sanity.config.js
@@ -2,6 +2,8 @@ import {defineConfig} from 'sanity';
 import {deskTool} from 'sanity/desk';
 import {schemaTypes} from './schemas';
 import {codeInput} from '@sanity/code-input';
+import { dashboardTool } from "@sanity/dashboard";
+import { netlifyWidget } from "sanity-plugin-dashboard-widget-netlify";
 
 const singleEdits = [
   { type: 'siteSettings' },
@@ -15,7 +17,29 @@ export default defineConfig({
   dataset: 'production',
   plugins: [
     deskTool(),
-    codeInput()
+    codeInput(),
+    dashboardTool({
+      widgets: [
+        netlifyWidget({
+            title: 'Sanity Netlify deploys',
+            sites: [
+              {
+                title: 'Sanity Studio',
+                apiId: '7899622d-3925-4716-824d-6ae101ed21d8',
+                buildHookId: 'https://api.netlify.com/build_hooks/63ab3fbf3258e44bbb4acba1',
+                name: 'ruhrpott-sanity-studio',
+              },
+              {
+                title: 'Website',
+                apiId: '7899622d-3925-4716-824d-6ae101ed21d8',
+                buildHookId: 'https://api.netlify.com/build_hooks/63ab3fbf3258e44bbb4acba1',
+                name: 'ruhrpott-sanity-website',
+                url: 'https://ruhrpott.netlify.app/',
+              }
+            ]
+        })
+      ]
+    })
   ],
   schema: {
     types: schemaTypes,
