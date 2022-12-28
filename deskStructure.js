53c418c5fbf22f0ba7d1684e39ce84a3c09f6d39
Werner Glinka <werner@glinka.co>
2022-12-28 17:18:53 -0600

    finished studio for now

--- deskStructure.js
+++ deskStructure.js
@@ -4,6 +4,7 @@ export const ruhrpottStructure = (S) =>
   S.list()
     .title('Root')
     .items([
+      // Add the settings documents first
       S.listItem()
         .title('Settings')
         .icon(FiTool)
@@ -22,22 +23,24 @@ export const ruhrpottStructure = (S) =>
           ])
         ),
       S.divider(),
-      S.listItem().title('All Authors').icon(FiUsers).child(
-        /* Create a list of all authors */
+      // add the data content types second
+      S.listItem().title('Authors').icon(FiUsers).child(
+        // Create a list of all authors
         S.documentList().title('All Authors').filter('_type == "authors"')
       ),
-      S.listItem().title('All Ruhrpott Cities').icon(FiGlobe).child(
-        /* Create a list of all posts */
-        S.documentList().title('All Cities').filter('_type == "cities"')
+      S.listItem().title('Ruhrpott Cities').icon(FiGlobe).child(
+        // Create a list of all posts
+        S.documentList().title('All Ruhrpott Cities').filter('_type == "cities"')
       ),
-      S.listItem().title('All Football Clubs').icon(FiDribbble).child(
-        /* Create a list of all football clubs */
-        S.documentList().title('All Authors').filter('_type == "footballClubs"')
+      S.listItem().title('Football Clubs').icon(FiDribbble).child(
+        // Create a list of all football clubs
+        S.documentList().title('All Football Clubs').filter('_type == "footballClubs"')
       ),
       S.divider(),
-  // Remove the all singletons from the main list
-  ...S.documentTypeListItems().filter(
-    (listItem) => !['siteSettings','navigation','authors','cities','footballClubs' ].includes(listItem.getId())
-  ),
-])
+      // Remove the all singletons and previous defined list items from the main list
+      // at thie point only blog piosts and pages remain to be listed
+      ...S.documentTypeListItems().filter(
+        (listItem) => !['siteSettings','navigation','authors','cities','footballClubs' ].includes(listItem.getId())
+      ),
+    ]);
 
--- sanity.config.js
+++ sanity.config.js
@@ -27,12 +27,6 @@ export default defineConfig({
             title: 'Sanity Netlify deploys',
             sites: [
               {
-                title: 'Sanity Studio',
-                apiId: '7899622d-3925-4716-824d-6ae101ed21d8',
-                buildHookId: '63ab3fbf3258e44bbb4acba1',
-                name: 'ruhrpott-sanity-studio',
-              },
-              {
                 title: 'Website',
                 apiId: '7899622d-3925-4716-824d-6ae101ed21d8',
                 buildHookId: '63ab3fbf3258e44bbb4acba1',
